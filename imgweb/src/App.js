621d710a32bfa6dbdd41f80d5ad5c610f4c6026c
Matthew Yu <matthewyu@b98-aruba-authenticated-10-110-152-60.wlan.cpp.edu>
2023-03-08 16:59:22 -0800

    Web files (React)

---
+++ imgweb/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TopPage from './components/TopPage';
+
+function App() {
+  return (
+    <div>
+      <TopPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ imgweb/src/components/TopPage.js
@@ -0,0 +1,8 @@
+import Paper from '@mui/material/Paper'
+const TopPage = () => {
+    return <Paper>
+        Hello
+    </Paper>
+}
+
+export default TopPage
