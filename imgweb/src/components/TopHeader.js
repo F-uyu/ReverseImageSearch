fe29387b1dfce1be35fbd7ae607c403daf612d8c
Matthew Yu <matthewyu@b98-aruba-authenticated-10-110-152-60.wlan.cpp.edu>
2023-03-08 19:56:51 -0800

    Added background picture + working on navbar

---
+++ imgweb/src/components/TopHeader.js
@@ -0,0 +1,20 @@
+//Navbar
+import React from "react";
+import './TopHeader.css';
+
+const TopHeader = () => {
+    return (
+        <div className = "header">
+            <div className = "header__left">
+                <img src = "" alt = "" />
+                <h2> Thing</h2>
+            </div>
+            <div className = "header__center">
+
+            </div>
+            <div className = "header__right">
+
+            </div>
+        </div>
+    )
+}
--- imgweb/src/components/TopPage.js
+++ imgweb/src/components/TopPage.js
@@ -1,8 +1,24 @@
+//Background picture
 import Paper from '@mui/material/Paper'
+import image from "../Images/SciFi.jpeg"
+import './TopPage.css'
 const TopPage = () => {
-    return <Paper>
-        Hello
-    </Paper>
+    return (
+        <>
+            <div style = {{
+                backgroundImage: `url(${image})`,
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                width: '100vw',
+                height: '100vh'
+
+            }}>
+
+                Hello World
+            </div>
+        </>
+    )
 }
 
 export default TopPage
